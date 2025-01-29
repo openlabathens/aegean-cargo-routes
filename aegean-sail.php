73cdfd21280eeb272b7e16f0416809f2c2c00276
Gregory <pastsar@gmail.com>
2025-01-29 14:04:24 +0200

    rewrite all funtionality as wp plugin

---
+++ aegean-sail.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: Aegean Sail
+ * Description: Add Custom Functionality
+ * Version: 1.0
+ * Author: Open Lab Athens
+ * Author URI: https://olathens.gr/
+ * Text Domain: aegean-sail
+ * License:      GPL2
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ **/
+
+// Register Post Types
+require_once('registration/register-post-types.php');
+
+//Template to show routes
+require_once('routes-template.php');
+
+//Helpers
+require_once('inc/routes-helpers.php');
+require_once('inc/routes-ajax.php');
+
+//Include Style
+function aegean_sail_enqueue_style() {
+	wp_enqueue_style( 'aegean-sail-routes', plugins_url('/css/aegean-sail.css', __FILE__) );
+	wp_enqueue_style( 'aegean-sail-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' );
+	wp_enqueue_style( 'aegean-sail-roundslider', 'https://cdn.jsdelivr.net/npm/round-slider@1.6.1/dist/roundslider.min.css' );
+	wp_enqueue_style( 'aegean-sail-font','https://fonts.googleapis.com/css2?family=Geologica:wght@100..900&display=swap" rel="stylesheet');
+
+}
+add_action( 'wp_enqueue_scripts', 'aegean_sail_enqueue_style' );
+
+
---
+++ inc/routes-helpers.php
@@ -0,0 +1,46 @@
+<?php
+
+function routes_map_direction($direction){
+
+    $direction_mapping = array(
+        'SE'=> __('SouthEast','aegean-sail'),
+        'S'=> __('South','aegean-sail'),
+        'SSE'=> __('SouthSouthEast','aegean-sail'),
+        'N'=> __('North','aegean-sail'),
+        'E'=> __('East','aegean-sail'),
+        'W'=> __('West','aegean-sail'),
+    );
+
+    return $direction_mapping[$direction];
+}
+
+function routes_map_wind_direction($windDirection){
+
+    $wind_direction_mapping = array(
+        '90'=> 'N',
+        '180'=> 'E',
+        '270'=> 'S',
+        '360'=> 'W'
+    );
+
+    return $wind_direction_mapping[$windDirection];
+}
+
+function routes_map_wind_speed($windSpeed){
+
+    $wind_speed_mapping = array(
+        '90'=> '0_5',
+        '135'=> '0_5',
+        '180'=> '6_10',
+        '225'=> '10_20',
+        '270'=> '10_20',
+        '315'=> '10_20',
+        '360'=> '20_30',
+        '45'=> '20_30',
+    );
+
+    return $wind_speed_mapping[$windSpeed];
+}
+
+
+?>
